9851897942c87425c0094c62b99fabd28d9e1f56
Chad Eddington <edd11001@byui.edu>
2016-10-13 21:08:28 -0600

    More filters for demo

--- public/assets/js/app/demo.module.js
+++ public/assets/js/app/demo.module.js
@@ -4,5 +4,7 @@ demo.controller('SampleCtrl', ['$scope', '$http', function($scope, $http) {
   $http.get('/data/data.json')
        .success(function(data) {
          $scope.data = data;
+         // pre-select first name from drop down
+         $scope.order = 'firstName';
        });
 }]);
