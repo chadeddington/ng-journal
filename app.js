1cad5514cd2a5764ef179c39261e2bbb700dc919
Chad Eddington <edd11001@byui.edu>
2016-10-13 19:26:48 -0600

    Initial commit

---
+++ app.js
@@ -0,0 +1,22 @@
+var express = require('express'),
+    bodyParser = require('body-parser'),
+    ejs = require('ejs'),   
+    app = express();
+
+app.set('views', './views');
+app.set('view engine', 'ejs');
+app.use(bodyParser.json());
+app.use(express.static(__dirname + "/views"));
+
+// Routes
+
+app.get('/', function(req, res) {
+  res.render('index');
+})
+
+// Start server
+var PORT = 8080;
+var server = app.listen(PORT, function () {
+    var port = server.address().port;
+    console.log("App now running on port", port);
+  });
