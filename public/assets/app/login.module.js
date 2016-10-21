03a8067ca1c6ed72bf8b98b4359d69d4ee4a494e
Chad Eddington <edd11001@byui.edu>
2016-10-20 22:36:26 -0600

    Add placeholder click functions

--- public/assets/app/login.module.js
+++ public/assets/app/login.module.js
@@ -1,5 +1,12 @@
 var login = angular.module('login', []);
 
 login.controller('LoginCtrl', ['$scope', '$http', function($scope, $http) {
+  $scope.signIn = function() {
+    console.log('Loggin in...');
+  }
+
+  $scope.register = function() {
+    console.log('Creating account...');
+  }
   
 }]);
